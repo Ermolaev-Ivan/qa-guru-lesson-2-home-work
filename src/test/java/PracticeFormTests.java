3a9c2ae5adcf50d4b0a9f34b14d6511ca5bbab7a
unknown <thedollmater2@gmail.com>
2021-12-17 21:05:37 +0300

    Homework completed

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -1,55 +1,53 @@
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.*;
+
 public class PracticeFormTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
 
     @Test
     void formTest() {
 
+
+
         Selenide.open("https://demoqa.com/automation-practice-form");
 
-
         Selenide.$("#firstName")
-                .setValue("gordon");
-
+                .setValue("Gordon");
         Selenide.$("#lastName")
-                .setValue("freeman");
-
+                .setValue("Freeman");
         Selenide.$("#userEmail")
                 .setValue("freeman_g@black.mesa");
-
         Selenide.$("label[for='gender-radio-1']")
                 .click();
-
         Selenide.$("#userNumber")
                 .setValue("9998887766");
-
         Selenide.$("#subjectsInput")
-                .setValue("physics")
+                .setValue("Physics")
                 .pressEnter();
-
         Selenide.$("#userNumber")
                 .setValue("9998887766");
 
 // Date of Birth
-
         Selenide.$("#dateOfBirthInput")
                 .click();
-
         Selenide.$(".react-datepicker__month-select")
                 .selectOptionContainingText("November");
-
         Selenide.$(".react-datepicker__year-select")
                 .selectOptionContainingText("1998");
-
         Selenide.$$("div[role='option']")
                 .findBy(Condition.text("19"))
                 .click();
 
-
+// Checkboxes
         Selenide.$("label[for='hobbies-checkbox-1']").
                 click();
         Selenide.$("label[for='hobbies-checkbox-2']")
@@ -59,29 +57,38 @@ public class PracticeFormTests {
 
 
 // Picture
-
         File file = new File("static/Gordon.jpg");
         Selenide.$("input[id='uploadPicture']").uploadFile(file);
 
 // State and city
-
         Selenide.$("textarea.form-control")
                 .setValue("10400 Northeast Fourth Street Floor 14 Bellevue, WA 98004 USA");
-
         Selenide.$("#react-select-3-input")
                 .setValue("Rajasthan")
                 .pressEnter();
-
         Selenide.$("#react-select-4-input")
                 .setValue("Jaiselmer")
                 .pressEnter();
 
 // Send form
-
         Selenide.$("#submit")
                 .scrollTo()
                 .click();
-        Selenide.sleep(22222);
+
+// Validation form
+        Selenide.$("#example-modal-sizes-title-lg").shouldBe(visible);
+
+        Selenide.$$("tbody tr > td:nth-child(2)").shouldHave(CollectionCondition.exactTexts("Gordon Freeman",
+                "freeman_g@black.mesa", "Male", "9998887766",
+                "19 November,1998", "Physics",
+                "Sports, Reading, Music", "Gordon.jpg",
+                "10400 Northeast Fourth Street Floor 14 Bellevue, WA 98004 USA","Rajasthan Jaiselmer"));
+
+// Close form
+        Selenide.$("#closeLargeModal")
+                .click();
+        Selenide.$("#example-modal-sizes-title-lg")
+                .shouldBe(not(visible));
 
     }
 }
