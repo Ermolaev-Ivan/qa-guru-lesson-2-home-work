60268a030379dc5737e2327dfc9bde2f9c9c94e7
unknown <thedollmater2@gmail.com>
2021-12-20 21:12:35 +0300

    updated

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -1,10 +1,12 @@
-import com.codeborne.selenide.*;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
 
@@ -15,80 +17,58 @@ public class PracticeFormTests {
 
     @Test
     void formTest() {
+        open("https://demoqa.com/automation-practice-form");
 
-
-
-        Selenide.open("https://demoqa.com/automation-practice-form");
-
-        Selenide.$("#firstName")
-                .setValue("Gordon");
-        Selenide.$("#lastName")
-                .setValue("Freeman");
-        Selenide.$("#userEmail")
-                .setValue("freeman_g@black.mesa");
-        Selenide.$("label[for='gender-radio-1']")
-                .click();
-        Selenide.$("#userNumber")
-                .setValue("9998887766");
-        Selenide.$("#subjectsInput")
-                .setValue("Physics")
-                .pressEnter();
-        Selenide.$("#userNumber")
-                .setValue("9998887766");
+        $("#firstName").setValue("Gordon");
+        $("#lastName").setValue("Freeman");
+        $("#userEmail").setValue("freeman_g@black.mesa");
+        $("label[for='gender-radio-1']").click();
+        $("#userNumber").setValue("9998887766");
+        $("#subjectsInput").setValue("Physics").pressEnter();
+        $("#userNumber").setValue("9998887766");
 
 // Date of Birth
-        Selenide.$("#dateOfBirthInput")
-                .click();
-        Selenide.$(".react-datepicker__month-select")
-                .selectOptionContainingText("November");
-        Selenide.$(".react-datepicker__year-select")
-                .selectOptionContainingText("1998");
-        Selenide.$$("div[role='option']")
-                .findBy(Condition.text("19"))
-                .click();
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionContainingText("November");
+        $(".react-datepicker__year-select").selectOptionContainingText("1998");
+        $$("div[role='option']").findBy(Condition.text("19")).click();
 
 // Checkboxes
-        Selenide.$("label[for='hobbies-checkbox-1']").
-                click();
-        Selenide.$("label[for='hobbies-checkbox-2']")
-                .click();
-        Selenide.$("label[for='hobbies-checkbox-3']")
-                .click();
-
+        $(byText("Sports")).click();
+        $(byText("Reading")).click();
+        $(byText("Music")).click();
 
 // Picture
-        File file = new File("static/Gordon.jpg");
-        Selenide.$("input[id='uploadPicture']").uploadFile(file);
+        $("input[id='uploadPicture']").uploadFromClasspath("Gordon.jpg");
 
 // State and city
-        Selenide.$("textarea.form-control")
-                .setValue("10400 Northeast Fourth Street Floor 14 Bellevue, WA 98004 USA");
-        Selenide.$("#react-select-3-input")
-                .setValue("Rajasthan")
-                .pressEnter();
-        Selenide.$("#react-select-4-input")
-                .setValue("Jaiselmer")
-                .pressEnter();
+        $("#currentAddress").setValue("10400 Northeast Fourth Street Floor 14 Bellevue, WA 98004 USA");
+        $("#react-select-3-input").setValue("Rajasthan").pressEnter();
+        $("#react-select-4-input").setValue("Jaiselmer").pressEnter();
 
 // Send form
-        Selenide.$("#submit")
-                .scrollTo()
-                .click();
+        $("#submit").scrollTo().click();
 
 // Validation form
-        Selenide.$("#example-modal-sizes-title-lg").shouldBe(visible);
-
-        Selenide.$$("tbody tr > td:nth-child(2)").shouldHave(CollectionCondition.exactTexts("Gordon Freeman",
-                "freeman_g@black.mesa", "Male", "9998887766",
-                "19 November,1998", "Physics",
-                "Sports, Reading, Music", "Gordon.jpg",
-                "10400 Northeast Fourth Street Floor 14 Bellevue, WA 98004 USA","Rajasthan Jaiselmer"));
+        $("#example-modal-sizes-title-lg").shouldBe(visible);
+        $(".table").shouldHave(text("Gordon Freeman"),
+                text("freeman_g@black.mesa"),
+                text("Male"),
+                text("9998887766"),
+                text("19 November,1998"),
+                text("Physics"),
+                text("Sports, Reading, Music"),
+                text("Gordon.jpg"),
+                text("10400 Northeast Fourth Street Floor 14 Bellevue, WA 98004 USA"),
+                text("Rajasthan Jaiselmer"));
+//        Вариант через коллекцию
+//        $$("tbody tr > td:nth-child(2)").shouldHave(CollectionCondition.exactTexts(
+//                "Gordon Freeman", "freeman_g@black.mesa", "Male", "9998887766", "19 November,1998", "Physics",
+//                "Sports, Reading, Music", "Gordon.jpg", "10400 Northeast Fourth Street Floor 14 Bellevue, WA 98004 USA",
+//                "Rajasthan Jaiselmer"));
 
 // Close form
-        Selenide.$("#closeLargeModal")
-                .click();
-        Selenide.$("#example-modal-sizes-title-lg")
-                .shouldBe(not(visible));
-
+        $("#closeLargeModal").click();
+        $("#example-modal-sizes-title-lg").shouldNotBe(visible);
     }
 }
